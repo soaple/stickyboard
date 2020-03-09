017fa59081d632d96dbe3fd08ac4b50cd9182d8b
Soaple <inje@soaple.io>
2020-03-09 20:37:55 +0900

    Add GoogleAnalytics integration.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,15 @@
 // For IE 11 support
 import 'react-app-polyfill/stable';
 import 'react-app-polyfill/ie11';
+// Google Analytics
+import ReactGA from 'react-ga';
+const GA_TRACKING_ID = process.env.GOOGLE_ANALYTICS_TRACKING_ID;
+if (GA_TRACKING_ID) {
+    const gaTrackingIdRegExp = new RegExp(/(UA-[0-9]*-[0-9])/i);
+    if (gaTrackingIdRegExp.test(GA_TRACKING_ID)) {
+        ReactGA.initialize(GA_TRACKING_ID);
+    }
+}
 // React
 import React from 'react';
 import ReactDOM from 'react-dom';
