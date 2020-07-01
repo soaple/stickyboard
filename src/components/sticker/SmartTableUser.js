2feea1059552778c93a7ba3c43164128e6ddb302
Soaple <inje@soaple.io>
2020-07-01 17:17:44 +0900

    Custom column for SmartTable added.

--- src/components/sticker/SmartTableUser.js
+++ src/components/sticker/SmartTableUser.js
@@ -1,6 +1,26 @@
 import React from 'react';
+import styled from 'styled-components';
+import Button from '@material-ui/core/Button';
 import { SmartTable, OrderMethod } from '@stickyboard/smart-table';
 import MyAppUserSchema from 'graphql/schemas/myapp_user';
+
+const Wrapper = styled.div``;
+
+function CustomColumn(props) {
+    return (
+        <Wrapper>
+            <Button
+                variant="outlined"
+                onClick={(event) => {
+                    event.stopPropagation();
+                    alert('Custom column button clicked!');
+                }}
+                color="primary">
+                Click
+            </Button>
+        </Wrapper>
+    );
+}
 
 function SmartTableUser(props) {
     const { colors } = props;
@@ -16,6 +36,12 @@ function SmartTableUser(props) {
                 date_joined: 'Date Joined',
                 last_online: 'Last Online',
             }}
+            customColumns={[
+                {
+                    headerTitle: 'Custom Column',
+                    component: CustomColumn,
+                },
+            ]}
             initialOrderColumn={'date_joined'}
             initialOrderMethod={OrderMethod.DESC}
         />
