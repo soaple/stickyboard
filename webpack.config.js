684b0022140c6872edda0f79840217154f42db15
Soaple <inje@soaple.io>
2020-07-21 19:58:13 +0900

    Webpack live reload plugin applied.

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
-// webpack.dev.js
+// webpack.config.js
 
 const webpack = require('webpack');
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const LiveReloadPlugin = require('webpack-livereload-plugin');
 const CompressionPlugin = require('compression-webpack-plugin');
 // const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
@@ -13,7 +14,7 @@ const NODE_ENV = process.env.NODE_ENV || 'production';
 const isProductionMode = NODE_ENV === 'production';
 const isWebpackDevServerMode = process.env.WEBPACK_DEV_SERVER_MODE === 'true';
 console.log('================ webpack.config.js ================');
-console.log(`process.env.NODE_ENV: ${process.env.NODE_ENV}`)
+console.log(`process.env.NODE_ENV: ${process.env.NODE_ENV}`);
 console.log(`isProductionMode: ${isProductionMode}`);
 console.log(`isWebpackDevServerMode: ${isWebpackDevServerMode}`);
 console.log('================================================');
@@ -39,61 +40,64 @@ const config = {
         publicPath: isWebpackDevServerMode ? '/' : '/dist/',
     },
     module: {
-        rules: [{
-            test: /\.jsx?$/,
-            exclude: /node_modules/,
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: ['@babel/preset-env', '@babel/preset-react'],
-                    plugins: [
-                        '@babel/plugin-proposal-class-properties',
-                        '@babel/plugin-proposal-export-default-from',
-                        '@babel/plugin-proposal-object-rest-spread',
-                        '@babel/plugin-syntax-dynamic-import',
-                        '@babel/plugin-transform-runtime',
-                        [
-                            'babel-plugin-transform-imports',
-                            {
-                                '@material-ui/core': {
-                                    'transform': '@material-ui/core/esm/${member}',
-                                    'preventFullImport': true
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react'],
+                        plugins: [
+                            '@babel/plugin-proposal-class-properties',
+                            '@babel/plugin-proposal-export-default-from',
+                            '@babel/plugin-proposal-object-rest-spread',
+                            '@babel/plugin-syntax-dynamic-import',
+                            '@babel/plugin-transform-runtime',
+                            [
+                                'babel-plugin-transform-imports',
+                                {
+                                    '@material-ui/core': {
+                                        transform:
+                                            '@material-ui/core/esm/${member}',
+                                        preventFullImport: true,
+                                    },
+                                    '@material-ui/icons': {
+                                        transform:
+                                            '@material-ui/icons/esm/${member}',
+                                        preventFullImport: true,
+                                    },
                                 },
-                                '@material-ui/icons': {
-                                    'transform': '@material-ui/icons/esm/${member}',
-                                    'preventFullImport': true
-                                }
-                            }
-                        ]
-                    ]
-                }
-            },
-        }, {
-            test: /\.css$/,
-            loaders: [
-                'style-loader',
-                'css-loader'
-            ]
-        }, {
-            test: /\.(ico|png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)$/,
-            use: {
-                loader: 'file-loader',
-                options: {
-                    name(file) {
-                        if (process.env.NODE_ENV === 'development') {
-                            return '[path][name].[ext]';
-                        }
-
-                        return '[contenthash].[ext]';
+                            ],
+                        ],
                     },
                 },
-            }
-        }]
+            },
+            {
+                test: /\.css$/,
+                loaders: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(ico|png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)$/,
+                use: {
+                    loader: 'file-loader',
+                    options: {
+                        name(file) {
+                            if (process.env.NODE_ENV === 'development') {
+                                return '[path][name].[ext]';
+                            }
+
+                            return '[contenthash].[ext]';
+                        },
+                    },
+                },
+            },
+        ],
     },
     resolve: {
         modules: ['src', 'node_modules'],
         alias: {
-            react: path.resolve('./node_modules/react')
+            react: path.resolve('./node_modules/react'),
         },
         symlinks: false,
     },
@@ -106,12 +110,13 @@ const config = {
             ...stickyboardConfig,
             template: 'src/view/index.ejs',
         }),
+        new LiveReloadPlugin({}),
         new CompressionPlugin({
-            filename: "[path].gz[query]",
-            algorithm: "gzip",
+            filename: '[path].gz[query]',
+            algorithm: 'gzip',
             test: /\.js$|\.css$|\.html$/,
             threshold: 10240,
-            minRatio: 0.8
+            minRatio: 0.8,
         }),
         // If you want to run bundle analyzer,
         // release below comments with require statements above
@@ -121,9 +126,9 @@ const config = {
         // })
     ],
     node: {
-        fs: 'empty'
-    }
-}
+        fs: 'empty',
+    },
+};
 
 if (!isProductionMode) {
     config['devtool'] = 'inline-source-map';
